7199a83b4a58f6d2e83f1b4b1e68d7a2cf361c28
Dan Richards <dan.m.richards@gmail.com>
2018-11-08 20:51:30 +0000

    Refactors handler register to store a name against each opcode handler. Opcode handlers now return the handled opcode value. Often different from the opcode itself. Go mod support.

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/danmrichards/chip8
+
+require github.com/veandco/go-sdl2 v0.0.0-20180925095440-75ff82abc4e3
--- opcodes.go
+++ opcodes.go
@@ -1,26 +1,65 @@
 package main
 
 import (
-	"github.com/pkg/errors"
+	"errors"
+	"fmt"
+	"log"
 )
 
-// opcodeHandler handles the opcode opc and returns an error if it occurred.
-type opcodeHandler func() error
+type opcodeHandler struct {
+	opcode  string
+	handler opcodeHandlerFunc
+}
+
+// opcodeHandlerFunc returns the handled value and an error if it occurred.
+// It is possible for the handler to derive a shifted opcode during processing
+// from the original opcode set on the chip8 itself.
+type opcodeHandlerFunc func() (uint16, error)
 
 // registerHandlers registers up the map of opcode handlers
 func (c *chip8) registerHandlers() {
 	// TODO: names for opcodes.
 	c.handlers = map[uint16]opcodeHandler{
-		0x0000: c.handle0x0000,
-		0x1000: c.jump,
-		0x2000: c.callSub,
-		0x3000: c.skipVxNN,
-		0x6000: c.setVx,
-		0x7000: c.incVx,
-		0x9000: c.skipVxNotVy,
-		0xA000: c.setAddress,
-		0xD000: c.drawDisp,
-		0xF000: c.handle0xF000,
+		0x0000: {
+			opcode:  "handle0x0000",
+			handler: c.handle0x0000,
+		},
+		0x1000: {
+			opcode:  "1NNN",
+			handler: c.jump,
+		},
+		0x2000: {
+			opcode:  "2NNN",
+			handler: c.callSub,
+		},
+		0x3000: {
+			opcode:  "3XNN",
+			handler: c.skipVxNN,
+		},
+		0x6000: {
+			opcode:  "6XNN",
+			handler: c.setVx,
+		},
+		0x7000: {
+			opcode:  "7XNN",
+			handler: c.incVx,
+		},
+		0x9000: {
+			opcode:  "9XY0",
+			handler: c.skipVxNotVy,
+		},
+		0xA000: {
+			opcode:  "ANNN",
+			handler: c.setAddress,
+		},
+		0xD000: {
+			opcode:  "DXYN",
+			handler: c.drawDisp,
+		},
+		0xF000: {
+			opcode:  "handle0xF000",
+			handler: c.handle0xF000,
+		},
 	}
 }
 
@@ -34,20 +73,24 @@ func (c *chip8) handle() error {
 	// indicates the action to take.
 	h, ok := c.handlers[c.opc&0xF000]
 	if !ok {
-		return errHandleOpcode{
-			opc: c.opc,
-			err: errors.New("unsupported opcode"),
-		}
+		return fmt.Errorf("unsupported opcode: 0x%X", c.opc)
 	}
 
-	// TODO: debug mode support. Log opcode and handler name.
+	// Handle the opcode.
+	v, err := h.handler()
+	if err != nil {
+		return fmt.Errorf("error handling opcode: %s value: 0x%X: %s", h.opcode, v, err)
+	}
 
-	return h()
+	// TODO: debug mode support.
+	log.Printf("opcode: %s value: 0x%X\n", h.opcode, v)
+
+	return nil
 }
 
 // handle0x0000 performs additional opcode parsing to determine the correct
 // action. Codes in this range cannot rely on the first 4 bits.
-func (c *chip8) handle0x0000() error {
+func (c *chip8) handle0x0000() (uint16, error) {
 	switch {
 	case c.opc&0x00FF == 0x00E0:
 		return c.clrDisp()
@@ -59,49 +102,40 @@ func (c *chip8) handle0x0000() error {
 		return c.callSys()
 
 	default:
-		return errHandleOpcode{
-			opc: c.opc,
-			err: errors.New("unknown opcode [0x0000]"),
-		}
+		return c.opc, errors.New("unknown opcode [0x0000]")
 	}
 }
 
 // callSys calls RCA 1802 program at address NNN. Not necessary for most ROMs.
-func (c *chip8) callSys() error {
-	return errHandleOpcode{
-		opc: c.opc,
-		err: errors.New("TODO: callSys"),
-	}
+func (c *chip8) callSys() (uint16, error) {
+	return c.opc & 0xF000, errors.New("TODO: callSys")
 }
 
 // clrDisp clears the display.
-func (c *chip8) clrDisp() error {
-	return errHandleOpcode{
-		opc: c.opc,
-		err: errors.New("TODO: clrDisp"),
-	}
+func (c *chip8) clrDisp() (uint16, error) {
+	return c.opc & 0x00FF, errors.New("TODO: clrDisp")
 }
 
 // subRet returns from a subroutine.
-func (c *chip8) subRet() error {
+func (c *chip8) subRet() (uint16, error) {
 	// Return to the program counter stored in the stack (adding 2 for the
 	// next instruction as usual).
 	c.pc = c.stack[c.sp] + 2
 	c.sp--
 
-	return nil
+	return c.opc & 0x00FF, nil
 }
 
 // jump jumps to address NNN.
-func (c *chip8) jump() error {
+func (c *chip8) jump() (uint16, error) {
 	// Jump to NNN.
 	c.pc = c.opc & 0x0FFF
 
-	return nil
+	return c.opc, nil
 }
 
 // callSub calls subroutine at NNN.
-func (c *chip8) callSub() error {
+func (c *chip8) callSub() (uint16, error) {
 	// Store the current program counter temporarily while we jump to
 	// the subroutine. Incrementing the stack pointer to prevent overwrite.
 	c.sp++
@@ -109,12 +143,12 @@ func (c *chip8) callSub() error {
 
 	// Jump to NNN.
 	c.pc = c.opc & 0x0FFF
-	return nil
+	return c.opc, nil
 }
 
 // skipVxNN skips the next instruction if VX equals NN. Usually the next instruction
 // is a jump to skip a code block.
-func (c *chip8) skipVxNN() error {
+func (c *chip8) skipVxNN() (uint16, error) {
 	x := (c.opc & 0x0F00) >> 8 // Reverse the shift.
 	nn := byte(c.opc & 0x00FF) // Get the last 2 chars.
 
@@ -126,34 +160,34 @@ func (c *chip8) skipVxNN() error {
 		c.pc += 2
 	}
 
-	return nil
+	return c.opc, nil
 }
 
 // setVx sets CPU register Vx (where x is A-E) to NN.
-func (c *chip8) setVx() error {
+func (c *chip8) setVx() (uint16, error) {
 	x := (c.opc & 0x0F00) >> 8 // Reverse the shift.
 	nn := byte(c.opc & 0x00FF) // Get the last 2 chars.
 
 	c.V[x] = nn
 	c.pc += 2
 
-	return nil
+	return c.opc, nil
 }
 
 // incVx adds NN to CPU register Vx.
-func (c *chip8) incVx() error {
+func (c *chip8) incVx() (uint16, error) {
 	x := (c.opc & 0x0F00) >> 8 // Reverse the shift.
 	nn := byte(c.opc & 0x00FF) // Get the last 2 chars.
 
 	c.V[x] += nn
 	c.pc += 2
 
-	return nil
+	return c.opc, nil
 }
 
 // skipVxNotVy skips the next instruction if VX doesn't equal VY. Usually the
 // next instruction is a jump to skip a code block.
-func (c *chip8) skipVxNotVy() error {
+func (c *chip8) skipVxNotVy() (uint16, error) {
 	x := (c.opc & 0x0F00) >> 8
 	y := (c.opc & 0x00F0) >> 4
 
@@ -165,14 +199,14 @@ func (c *chip8) skipVxNotVy() error {
 		c.pc += 2
 	}
 
-	return nil
+	return c.opc, nil
 }
 
 // setAddress sets the index register to the address NNN.
-func (c *chip8) setAddress() error {
+func (c *chip8) setAddress() (uint16, error) {
 	c.I = c.opc & 0x0FFF
 	c.pc += 2 // Increase by 2 because each instruction is 2 bytes long.
-	return nil
+	return c.opc, nil
 }
 
 // drawDisp draws a sprite at coordinate (VX, VY) that has a width of 8 pixels
@@ -180,7 +214,7 @@ func (c *chip8) setAddress() error {
 // from memory location I; I doesn't change after the execution of this
 // instruction. VF is set to 1 if any screen pixels are flipped from set to
 // unset when the sprite is drawn, and to 0 if that doesn't happen.
-func (c *chip8) drawDisp() error {
+func (c *chip8) drawDisp() (uint16, error) {
 	x := uint16(c.V[(c.opc&0x0F00)>>8])
 	y := uint16(c.V[(c.opc&0x00F0)>>4])
 	height := c.opc & 0x000F
@@ -206,14 +240,32 @@ func (c *chip8) drawDisp() error {
 	c.draw = true
 	c.pc += 2
 
-	return nil
+	return c.opc, nil
 }
 
 // handle0xF000 performs additional opcode parsing to determine the correct
 // action. Codes in this range cannot rely on the first 4 bits.
-func (c *chip8) handle0xF000() error {
-	return errHandleOpcode{
-		opc: c.opc,
-		err: errors.New("TODO: handle0xF000"),
+func (c *chip8) handle0xF000() (uint16, error) {
+	switch c.opc & 0x00FF {
+	case 0x0033:
+		return c.setBCD()
 	}
+
+	return c.opc & 0xFFFF, errors.New("TODO: handle0xF000")
+}
+
+// setBCD stores the binary code decimal representation of VX with the most
+// significant of three digits at the address in I, the middle digit at I plus
+// 1, and the least significant digit at I plus 2. (In other words, take the
+// decimal representation of VX, place the hundreds digit in memory at location
+// in I, the tens digit at location I+1, and the ones digit at location I+2).1
+func (c *chip8) setBCD() (uint16, error) {
+	x := (c.opc & 0x0F00) >> 8
+
+	c.memory[c.I] = c.V[x] / 100          // Hundreds.
+	c.memory[c.I+1] = (c.V[x] / 10) % 10  // Tens.
+	c.memory[c.I+2] = (c.V[x] % 100) % 10 // Ones.
+	c.pc += 2
+
+	return c.opc & 0xFFFF, nil
 }
